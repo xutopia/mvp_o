88a8a07a78bd171e0d7da4019704752ebc2a2148
xutopia <xuology@gmail.com>
2017-01-16 18:47:35 -0500

    built filler div that should render to page. Checking webpack/babel is correct or not

--- front-end/components/app.js
+++ front-end/components/app.js
@@ -1,1 +1,12 @@
 // the main route for the homepage
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
--- front-end/components/nodes_new.js
+++ front-end/components/nodes_new.js
@@ -1,1 +1,14 @@
 // class with redux-form to make queries
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {Link} from 'react-router';
+
+class NodesNew extends Component {
+  render() {
+    const {fields: {name, owe}, handleSubmit} = this.props;
+    return (
+      <div>Some text
+      </div>
+    );
+  }
+}
